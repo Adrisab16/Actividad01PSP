c7231c77380da5301ab67828b03f5b0ec25c4998
Adrián <adrsabper@gmail.com>
2023-10-16 16:36:00 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Declaro variables
+int x;
+int y;
+int radio;
+int alto;
+int ancho;
+
+interface Igrafico{
+    public bool mover(int x, int y);
+    public string dibujar();
+} 
+
+
+
+internal class Punto{
+
+}
