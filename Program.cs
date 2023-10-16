a091b09dce59d146814e1239c11793f571b3ab3f
Adrián <adrsabper@gmail.com>
2023-10-16 17:26:02 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,18 +1,66 @@
-﻿// Declaro variables
-int x;
-int y;
-int radio;
-int alto;
-int ancho;
+﻿using System.Runtime.InteropServices;
 
-interface Igrafico{
+internal class EditorGrafico: IGrafico
+{
+    string IGrafico.dibujar(int x, int y)
+    {
+        if (x > 800 || y > 600){
+            throw new Exception("Te has salido de la pantalla (800x600)");
+        }
+
+        Console.Write(x.ToString(),y);
+
+        throw new NotImplementedException();
+    }
+
+    public bool mover(int x, int y)
+    {
+        if (x > 800 || y > 600)
+        {
+            return false;
+        }
+        throw new NotImplementedException();
+    }
+}
+
+interface IGrafico
+{
     public bool mover(int x, int y);
-    public string dibujar();
+    public string dibujar(int x, int y);
 } 
 
-
-
 internal class Punto{
-    int x;
-    int y;
+   private readonly int x;
+   private readonly int y;
+   public Punto(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+internal class GraficoCompuesto
+{
+
+}
+
+internal class Circulo
+{
+    private readonly int radio;
+    public Circulo(int x, int y, int radio)
+    {
+        this.radio = radio;
+    }
+}
+
+internal class Rectangulo
+{
+    private int ancho;
+    private int alto;
+    public Rectangulo(int x, int y, int ancho, int alto)
+    {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
 }
